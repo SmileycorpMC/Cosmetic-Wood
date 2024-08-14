d941cedb830ed2a205cb4b116c09ee4d8debe1f3
Smileycorp <callum.j.longbottom@gmail.com>
2024-08-14 21:35:24 +0100

    add rustic chairs

---
+++ src/main/java/net/smileycorp/cosmeticwood/plugins/minecraft/PluginRustic.java
@@ -0,0 +1,13 @@
+package net.smileycorp.cosmeticwood.plugins.minecraft;
+
+import net.minecraft.util.ResourceLocation;
+import net.smileycorp.cosmeticwood.api.CWPlugin;
+import net.smileycorp.cosmeticwood.api.WoodRegistryEntry;
+
+@CWPlugin(modid = "rustic")
+public class PluginRustic {
+    
+    public static final WoodRegistryEntry CHAIR = new WoodRegistryEntry.Builder(new ResourceLocation("rustic:chair_oak"))
+            .exclude("minecraft", "rustic").build();
+    
+}
