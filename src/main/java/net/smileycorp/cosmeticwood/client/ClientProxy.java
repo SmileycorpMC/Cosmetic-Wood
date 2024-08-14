2734b989881bef8037441a990194731c9d5568e9
Smileycorp <callum.j.longbottom@gmail.com>
2024-08-14 21:34:28 +0100

    fix block drops

--- src/main/java/net/smileycorp/cosmeticwood/client/ClientProxy.java
+++ src/main/java/net/smileycorp/cosmeticwood/client/ClientProxy.java
@@ -1,8 +1,6 @@
 package net.smileycorp.cosmeticwood.client;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.StateMap;
@@ -21,10 +19,7 @@ import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -42,7 +37,6 @@ import net.smileycorp.cosmeticwood.common.registry.item.WoodStack;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 @SideOnly(Side.CLIENT)
 @EventBusSubscriber(value=Side.CLIENT, modid = Constants.MODID)
--- src/main/java/net/smileycorp/cosmeticwood/mixin/MixinBlock.java
+++ src/main/java/net/smileycorp/cosmeticwood/mixin/MixinBlock.java
@@ -1,11 +1,12 @@
 package net.smileycorp.cosmeticwood.mixin;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -13,9 +14,10 @@ import net.minecraft.world.World;
 import net.smileycorp.cosmeticwood.common.data.WoodHandler;
 import net.smileycorp.cosmeticwood.common.data.WoodTypeStorage;
 import net.smileycorp.cosmeticwood.common.registry.block.ModifiableWoodBlock;
+import net.smileycorp.cosmeticwood.common.registry.block.WoodBlock;
+import net.smileycorp.cosmeticwood.common.registry.item.WoodItem;
 import net.smileycorp.cosmeticwood.common.registry.item.WoodStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,8 +25,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Block.class)
 public abstract class MixinBlock implements ModifiableWoodBlock {
-    
-    @Shadow protected abstract BlockStateContainer createBlockState();
     
     private boolean isWood;
     private ResourceLocation defaultType = WoodHandler.getDefault();
@@ -74,6 +74,17 @@ public abstract class MixinBlock implements ModifiableWoodBlock {
     @Inject(at = @At("HEAD"), method = "getBlockLayer", cancellable = true)
     public void CW$getBlockLayer(CallbackInfoReturnable<BlockRenderLayer> callback) {
         if (isWood()) callback.setReturnValue(BlockRenderLayer.CUTOUT_MIPPED);
+    }
+    
+    @Inject(at = @At("RETURN"), method = "getDrops(Lnet/minecraft/util/NonNullList;Lnet/minecraft/world/IBlockAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;I)V", remap = false)
+    public void CW$getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune, CallbackInfo callback) {
+        if (!((WoodBlock)state).isWood()) return;
+        for (int i = 0; i < drops.size(); i++) {
+            ItemStack stack = drops.get(i);
+            if (!((WoodStack)(Object)stack).isWoodItem()) continue;
+            if (ItemBlock.getItemFromBlock(state.getBlock()) != stack.getItem()) continue;
+            drops.set(i, WoodItem.getStack(stack, WoodTypeStorage.getWoodType(world, pos)));
+        }
     }
     
 }
