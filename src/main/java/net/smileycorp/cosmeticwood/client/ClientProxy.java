5aefb05ecb6b3220c7edd9e9e97edab47501de45
Smileycorp <callum.j.longbottom@gmail.com>
2024-09-20 16:29:38 +0100

    add wood type to f3 screen

--- src/main/java/net/smileycorp/cosmeticwood/client/ClientProxy.java
+++ src/main/java/net/smileycorp/cosmeticwood/client/ClientProxy.java
@@ -1,7 +1,9 @@
 package net.smileycorp.cosmeticwood.client;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.color.BlockColors;
@@ -9,10 +11,14 @@ import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.registry.IRegistry;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -22,6 +28,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.smileycorp.atlas.api.util.TextUtils;
+import net.smileycorp.cosmeticwood.api.registry.block.WoodBlock;
 import net.smileycorp.cosmeticwood.api.registry.item.WoodStack;
 import net.smileycorp.cosmeticwood.common.CommonProxy;
 import net.smileycorp.cosmeticwood.common.Constants;
@@ -88,6 +95,22 @@ public class ClientProxy extends CommonProxy {
 			tooltip.add(TextUtils.toProperCase(type));
 		}
 		else tooltip.add(TextUtils.toProperCase(WoodHandler.getDefault().getResourcePath()));
+	}
+	
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public static void renderText(RenderGameOverlayEvent.Text event) {
+		Minecraft mc = Minecraft.getMinecraft();
+		if (!mc.gameSettings.showDebugInfo) return;
+		RayTraceResult result = mc.objectMouseOver;
+		if (result == null) return;
+		if (result.typeOfHit != RayTraceResult.Type.BLOCK) return;
+		BlockPos pos = result.getBlockPos();
+		if (pos == null) return;
+		WorldClient world = mc.world;
+		IBlockState state = world.getBlockState(pos);
+		if (!((WoodBlock) state).isWood()) return;
+		ResourceLocation type = WoodTypeStorage.getWoodType(world, pos);
+		event.getRight().add("wood_type: " + type);
 	}
 	
 	public static void syncChunk(int x, int z, NBTTagCompound nbt) {
