237ee714a864eeabbcb888a6b7c82aadb2a2c6c5
Smileycorp <callum.j.longbottom@gmail.com>
2024-09-21 22:03:25 +0100

    make content work on a dedicated server

---
+++ src/main/java/net/smileycorp/cosmeticwood/mixin/MixinMinecraftServer.java
@@ -0,0 +1,26 @@
+package net.smileycorp.cosmeticwood.mixin;
+
+import com.mojang.authlib.GameProfileRepository;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
+import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerProfileCache;
+import net.minecraft.util.datafix.DataFixer;
+import net.smileycorp.cosmeticwood.common.registry.ContentRegistry;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.File;
+import java.net.Proxy;
+
+@Mixin(MinecraftServer.class)
+public class MixinMinecraftServer {
+    
+    @Inject(at = @At("TAIL"), method = "<init>")
+    public void cw$init(File world, Proxy proxy, DataFixer fixer, YggdrasilAuthenticationService authService, MinecraftSessionService sessionService, GameProfileRepository profileRepository, PlayerProfileCache profileCache, CallbackInfo callback) {
+        ContentRegistry.generateData();
+    }
+    
+}
